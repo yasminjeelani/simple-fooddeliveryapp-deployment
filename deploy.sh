8e4a62628b76d373f6176280b7e56178c56b5e48
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2024-02-14 14:46:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 docker-compose up -d
 
 #tagging the image:
-docker tag nodejs:web ravivarman46/nodejs:cicd
+docker tag nodejs:web yasmin/nodejs:cicd
 
 #pushing an image to dockerhub:
-docker push ravivarman46/nodejs:cicd
+docker push yasmin/nodejs:cicd
 echo "the image has been pushed to docker hub"
